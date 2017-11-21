efea2e0f90749365433bc3f91d80f20e96e861fb
k_a_a_3 <k_a_a_3@mail.ru>
2017-11-21 22:06:39 +0300

    my first commit изучали типы данных

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        int a;
+        int b;
+        b=8;
+        a=5;
 	// write your code here
+        System.out.println(a);
+        a=a+5;
+        if(a<b){
+            a+=b;
+        }
+
+        System.out.println(a);
         System.out.print("Здравствуй мир");
 
     }
