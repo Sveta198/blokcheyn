21154b02b5b2d7b0f42fb7c67fa5af7266fc38ff
k_a_a_3 <k_a_a_3@mail.ru>
2017-11-22 20:33:16 +0300

    my first commit изучали типы данных

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,6 +18,15 @@ public class Main {
         System.out.print("Здравствуй мир");
         int[] array = {6, 9, 3, 25, 96, 47};
         int m = 999;
+        int var1=5;
+        int var2=8;
+        int var3;
+        var3=var1;
+        var1=var2;
+        var2=var3;
+        System.out.println(var1);
+        System.out.println(var2);
 
     }
 }
+
